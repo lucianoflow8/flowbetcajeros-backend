6b8afa7eb828b28d029a20a9b350c5c400386022
lucianoflow8 <maximilianoluciano2006@gmail.com>
2025-07-28 22:13:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import requests
 
 app = FastAPI()
 
+# Credenciales
 USERNAME = "rosario"
 PASSWORD = "luciano151418"
 CLIENT_ID = "1_5i50wo24kpcscc0okw0ww4gsc8kwg0k8gs0ok44skooww4swcg"
 CLIENT_SECRET = "18qxs6584gw08scg8wsk8gow44oc4gcw40c4o8w44880g0gkcg"
+LOGIN_ID = 2017  # Este es tu ID como cajero
+SITE_ID = "86240"
 
 def get_token():
     url = "https://admin.flowbets.co/oauth/v2/token"
@@ -28,22 +31,30 @@ def get_token():
 
 @app.post("/crear_usuario")
 def crear_usuario(username: str = Form(...), password: str = Form(...)):
-    print(f"Creando usuario: {username}")
     token = get_token()
     if not token:
-        return {"error": "No se pudo obtener el token. Revisá credenciales o login."}
+        return {"error": "No se pudo obtener el token."}
 
     headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
+        "origin": "https://panel-skin2.jcasino.live",
+        "referer": "https://panel-skin2.jcasino.live/",
+        "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+        "content-type": "application/json"
     }
 
     payload = {
-        "UserName": username,
-        "Password": password,
-        "ConfirmPassword": password,
-        "PhoneNumber": "",
-        "Email": ""
+        "username": username,
+        "email": "",
+        "firstname": "-",
+        "password": password,
+        "phone": "",
+        "login_Id": LOGIN_ID,
+        "site": SITE_ID,
+        "token": token,
+        "proveedores": {
+            "poker": {"comision": 0, "status": True},
+            "casinolive": {"comision": 0, "status": True}
+        }
     }
 
     try:
@@ -51,7 +62,6 @@ def crear_usuario(username: str = Form(...), password: str = Form(...)):
         res.raise_for_status()
         return res.json()
     except Exception as e:
-        print("Error al crear usuario:", e)
         return {
             "error": "Error en la creación",
             "detalle": str(e),
