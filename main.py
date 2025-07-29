ef36e3e87fae495041f93fae10dbd2d7e170469a
lucianoflow8 <maximilianoluciano2006@gmail.com>
2025-07-28 21:41:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ app = FastAPI()
 
 USERNAME = "rosario"
 PASSWORD = "luciano151418"
+CLIENT_ID = "1_5i50wo24kpcscc0okw0ww4gsc8kwg0k8gs0ok44skooww4swcg"
+CLIENT_SECRET = "18qxs6584gw08scg8wsk8gow44oc4gcw40c4o8w44880g0gkcg"
 
 def get_token():
-    token_url = "https://agentes.flowbets.co/token"
+    login_url = "https://admin.flowbets.co/oauth/v2/token"
     params = {
         "username": USERNAME,
         "password": PASSWORD,
-        "client_id": "1_5i80w24kpcssc0okw0ww4gsc8kwg0k8gs0ok44skooww4swcg",
-        "client_secret": "18qxs6584gw08scg8wsk8gow44oc4gcw40c4o8w44880g0kgcg",
-        "grant_type": "password"
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "grant_type": "password",
+        "source": "pn"
     }
     try:
-        response = requests.get(token_url, params=params)
+        response = requests.get(login_url, params=params)
         print("Login status:", response.status_code)
         print("Login response:", response.text)
         response.raise_for_status()
-        return response.json().get("token")  # OJO: el campo se llama "token"
+        return response.json().get("access_token")
     except Exception as e:
         print("Error al obtener el token:", e)
         return None
